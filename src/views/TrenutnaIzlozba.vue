9eb97f3ca4eb7397fd850021f930ba534d21d95e
MarBlazek <148615785+MarBlazek@users.noreply.github.com>
2024-06-10 13:09:54 +0200

    komentari

--- src/views/TrenutnaIzlozba.vue
+++ src/views/TrenutnaIzlozba.vue
@@ -4,20 +4,27 @@
       <div class="image-grid">
         <div v-for="image in exhibitImages" :key="image.url" class="image-item">
           <img :src="image.url" :alt="image.name">
+          <div class="comments-section">
+            <div v-for="comment in image.comments" :key="comment.id" class="comment">
+              {{ comment.text }}
+            </div>
+            <input v-model="newComments[image.name]" @keyup.enter="addComment(image.name)" placeholder="Add a comment...">
+          </div>
         </div>
       </div>
     </div>
   </template>
-
+  
   <script>
   import { db } from '@/firebase';
-  import { doc, getDoc, collection, getDocs } from 'firebase/firestore';
+  import { doc, getDoc, collection, getDocs, addDoc } from 'firebase/firestore';
   
   export default {
     data() {
       return {
         exhibit: {},
-        exhibitImages: []
+        exhibitImages: [],
+        newComments: {}
       };
     },
     async created() {
@@ -30,14 +37,37 @@
         if (exhibitDoc.exists()) {
           this.exhibit = exhibitDoc.data();
           this.exhibitImages = this.exhibit.images;
+          for (let image of this.exhibitImages) {
+            image.comments = await this.fetchComments(image.name);
+          }
         } else {
           console.error('No such document!');
+        }
+      },
+      async fetchComments(imageName) {
+        const comments = [];
+        const commentsSnapshot = await getDocs(collection(db, 'comments', imageName, 'imageComments'));
+        commentsSnapshot.forEach(doc => {
+          comments.push({ id: doc.id, ...doc.data() });
+        });
+        return comments;
+      },
+      async addComment(imageName) {
+        const newComment = this.newComments[imageName];
+        if (newComment) {
+          await addDoc(collection(db, 'comments', imageName, 'imageComments'), { text: newComment });
+          this.newComments[imageName] = '';
+          // Refresh comments
+          const image = this.exhibitImages.find(img => img.name === imageName);
+          if (image) {
+            image.comments = await this.fetchComments(imageName);
+          }
         }
       }
     }
   };
   </script>
-
+  
   <style scoped>
   .exhibit {
     color: white;
@@ -45,22 +75,38 @@
     background-color: black;
     min-height: 100vh;
     position: relative;
-}
-.image-grid {
-display: flex;
-flex-wrap: wrap;
-justify-content: center;
-align-items: flex-start;
-gap: 10px;
-}
-.image-item {
-width: calc(25% - 10px);
-margin-bottom: 10px;
-}
-.image-item img {
-width: 100%;
-max-height: 200px;
-object-fit: cover;
-border-radius: 5px;
-}
-</style>
+  }
+  .image-grid {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: flex-start;
+    gap: 10px;
+  }
+  .image-item {
+    width: calc(25% - 10px);
+    margin-bottom: 10px;
+  }
+  .image-item img {
+    width: 100%;
+    max-height: 200px;
+    object-fit: cover;
+    border-radius: 5px;
+  }
+  .comments-section {
+    margin-top: 10px;
+  }
+  .comment {
+    background-color: white;
+    color: black;
+    padding: 5px;
+    border-radius: 5px;
+    margin-bottom: 5px;
+  }
+  input {
+    width: 100%;
+    padding: 5px;
+    border: none;
+    border-radius: 5px;
+  }
+  </style>
